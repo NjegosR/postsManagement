52710f7db36670d59a7ff80aecdeda4e320e0694
NjegosR <njegosr406@gmail.com>
2018-07-31 20:27:19 +0200

    I finished post-list-item component

--- src/app/post/post-list-item/post-list-item.component.ts
+++ src/app/post/post-list-item/post-list-item.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-post-list-item',
+  selector: '[app-post-list-item]',
   templateUrl: './post-list-item.component.html',
   styleUrls: ['./post-list-item.component.scss']
 })
