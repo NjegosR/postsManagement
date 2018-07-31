b1a7963e7fd654d6523ab371a52d9f9a3216a5a5
Vlado Tešanović <vlado.tesanovic@gmail.com>
2018-07-31 10:00:26 +0200

    refactoring

---
+++ src/app/shared/components/comment/comment.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CommentComponent implements OnInit {
+
+  @Input() name: string;
+  @Input() body: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
